297d2c78f6307e3423353e62b4af94d29bd837e5
soybean15 <marlonpadilla1593@gmail.com>
2023-10-17 17:51:49 +0800

    will create isCOmpleteProfile middleware

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum','verified'])->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 
    
